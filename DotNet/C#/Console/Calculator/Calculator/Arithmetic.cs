b9e9745e69dc5e7a40e8c8a7f9c040913c1a2a9d
Aniruddha Ramane <ramaneaniruddh22@gmail.com>
2024-11-17 21:52:16 +0530

    .net started (calculator app)

---
+++ DotNet/C#/Console/Calculator/Calculator/Arithmetic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Arithmetic
+    {
+        public int AddTwoNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public int MultiplyTwoNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public int SubtractTwoNumbers(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public int Divide(int firstNumber, int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}
---
+++ DotNet/C#/Console/Calculator/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 10;
+            int secondNumber = 20;
+
+            Arithmetic arithmeticObj = new Arithmetic();
+
+            Console.WriteLine("Addition of two numbers is: " + arithmeticObj.AddTwoNumbers(firstNumber, secondNumber));
+
+            Console.WriteLine("Subtraction of two numbers is: " + arithmeticObj.SubtractTwoNumbers(firstNumber, secondNumber));
+
+            Console.WriteLine("Multiplication of two numbers is: " + arithmeticObj.MultiplyTwoNumbers(firstNumber, secondNumber));
+
+            Console.WriteLine("Division of two numbers is: " + arithmeticObj.Divide(firstNumber, secondNumber));
+
+        }
+
+    }
+}
