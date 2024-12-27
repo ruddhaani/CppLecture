98b3e507f9503541400cf00e923134e84f8050e4
Aniruddha Ramane <ramaneaniruddh22@gmail.com>
2024-12-27 20:11:34 +0530

    events

---
+++ DotNet/C#/Console/EventExample1/EventExample1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publisherObj = new Publisher();
+            Subscriber subscriberObj = new Subscriber();
+
+            publisherObj.getMessageEvent += subscriberObj.Message;
+
+            MyEventHandler eventHandlerObj = publisherObj.RaiseEvent;
+
+            eventHandlerObj("Subscriber received the message");
+        }
+    }
+}
---
+++ DotNet/C#/Console/EventExample1/EventExample1/Publisher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample1
+{
+    delegate void MyEventHandler(string name);
+    internal class Publisher
+    {
+        public event MyEventHandler getMessageEvent;
+
+        public void RaiseEvent(string message)
+        {
+            getMessageEvent?.Invoke(message);
+        }
+    }
+}
